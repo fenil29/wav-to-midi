00d796f82b1dafdb18cd7591337a9fdf353ab69d
fenil29 <fenilkaneria@gmail.com>
2022-06-07 14:16:17 -0400

    prevent caching file for development

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# prevent caching file for development
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-@app.route('/upload')
 def upload_file_page():
     return render_template('./upload.html')
 
