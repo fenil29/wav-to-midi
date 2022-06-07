c9cd97827bc73836c89d8330e5163c14de64c31f
Manat Jyot Singh <90211273+Manat-singh@users.noreply.github.com>
2022-06-07 14:24:49 -0400

    added code to convert wav to midi

---
+++ backend/handlers/handlers.py
@@ -0,0 +1,14 @@
+from businesslogic.wav_to_midi import WavToMidi
+
+
+class Test:
+
+    def __int__(self):
+        return
+
+    def convert_file(self):
+        WavToMidi.convert_file('./sample3.wav', 'sample.mid')
+
+
+t = Test()
+t.convert_file()
